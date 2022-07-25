16893ddeeaa2e44e99d00c94bbebce3916ea04e7
Anthony Baraja <atbaraja@gmail.com>
2022-07-25 15:03:12 -0700

    Added Files for Energy Drink Store

---
+++ Driver.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import models.EnergyDrink;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String userInput = "";
+		Scanner scanner = new Scanner(System.in);
+		
+		// Starting Energy Drink List
+		ArrayList<EnergyDrink> drinkList = new ArrayList<EnergyDrink>();
+		drinkList.add(new EnergyDrink("Red Bull", 2.00));
+		drinkList.add(new EnergyDrink("Monster", 3.00));
+		
+		// menu interface
+		// while the userInput is not "x" we loop, we present the menu again
+		do {
+			System.out.println("Welcome to the Energy Drink Store!");
+			System.out.println("[1] Display List");
+			System.out.println("[2] Create and Add an Energy Drink to the list");
+			System.out.println("[x] Exit Store");
+			
+			userInput = scanner.nextLine();
+			
+			switch(userInput) {
+			case "1":
+				System.out.println("The Energy Drinks currently listed are:");
+				for(EnergyDrink currentDrink: drinkList) {
+					//System.out.println(currentDrink.getName() + " priced at: " + currentDrink.price);
+					System.out.printf("%s priced at: $%.2f \n", currentDrink.getName(), currentDrink.price);
+				}
+				break;
+			case "2":
+				// Get User's Drink Name
+				System.out.println("Enter new Energy Drink name: ");
+				String name = scanner.nextLine();
+				
+				// Get User's Drink Price
+				System.out.println("Enter the Energy Drink's price: ");
+				String priceString = scanner.nextLine();
+				try {
+					double priceDouble = Double.parseDouble(priceString);//Integer.parseInt(priceString);
+					if(priceDouble > 0) {
+						EnergyDrink newDrink = new EnergyDrink(name, priceDouble);
+						drinkList.add(newDrink);
+						System.out.println("Drink successfully added!");
+					} else {
+						System.out.println("Invalid Price, try again please.");
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					System.out.println(e.getMessage());
+				}
+
+				break;
+			case "x":
+				System.out.println("");
+				break;
+			default:
+				System.out.println("Wrong input. Please try again using one of the valid options.");
+				break;
+			}
+			
+		} while(!(userInput.equals("x")));
+
+	}
+
+}
